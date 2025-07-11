a17a1b74063946a0fd973e9a364952174740ec2b
Abheek Dhawan <abheekd@protonmail.com>
2025-07-10 21:48:22 -0500

    feat: add start and stop and update display

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # 🔒 Change to specific domains in production
+    allow_origins=["*"],  # 🔒 Change to specific domains in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
